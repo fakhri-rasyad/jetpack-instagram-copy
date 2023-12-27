4a83434a9c63402c2356917f597e110d4516b2b3
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-12-27 12:54:13 +0800

    Penambahan data model

---
+++ app/src/main/java/com/d121211017/instagramcopy/data/model/posts.kt
@@ -0,0 +1,3 @@
+package com.d121211017.instagramcopy.data.model
+
+class posts : ArrayList<postsItem>()
---
+++ app/src/main/java/com/d121211017/instagramcopy/data/model/postsItem.kt
@@ -0,0 +1,11 @@
+package com.d121211017.instagramcopy.data.model
+
+data class postsItem(
+    val date_created: String,
+    val deskripsi: String,
+    val id: String,
+    val image: String,
+    val likes: Int,
+    val name: String,
+    val profile_picture: String
+)
