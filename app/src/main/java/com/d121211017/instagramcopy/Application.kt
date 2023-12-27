4afd08d8797138385e0782285bc7967637e17085
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-12-27 15:47:27 +0800

    Pembuatan Application

--- app/src/main/java/com/d121211017/instagramcopy/Application.kt
+++ app/src/main/java/com/d121211017/instagramcopy/Application.kt
@@ -1,4 +1,14 @@
 package com.d121211017.instagramcopy
 
-class Application {
+import android.app.Application
+import com.d121211017.instagramcopy.data.Container
+import com.d121211017.instagramcopy.data.DefaultAppContainer
+
+class InstagramApplication(): Application() {
+    lateinit var container: Container
+
+    override fun onCreate(){
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
 }
