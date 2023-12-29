9ba0e1d4d61b09df6402e2d160a177115b9a034d
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-12-29 17:34:16 +0800

    Pembuatan navigasi untuk bottom app bar

--- app/src/main/java/com/d121211017/instagramcopy/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/d121211017/instagramcopy/ui/screen/main/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    InstagramCopyApp(viewModel = viewModel)
                 }
             }
         }
--- app/src/main/java/com/d121211017/instagramcopy/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/d121211017/instagramcopy/ui/screen/main/MainScreen.kt
@@ -1,25 +1,128 @@
 package com.d121211017.instagramcopy.ui.screen.main
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.d121211017.instagramcopy.R
+import com.d121211017.instagramcopy.ui.state.UiState
+import com.d121211017.instagramcopy.ui.theme.InstagramCopyTheme
 import com.d121211017.instagramcopy.ui.viewmodel.InstagramCopyViewModel
 
 enum class InstagramCopyScreen(val title: String){
     Home(title = "Home Screen"),
     Explore(title = "Explore Screen"),
     Profile(title = "Profile Screen"),
-
 }
 @Composable
 fun InstagramCopyApp(
     viewModel: InstagramCopyViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
     navController: NavHostController = rememberNavController()
 ){
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = InstagramCopyScreen.valueOf(backStackEntry?.destination?.route ?: InstagramCopyScreen.Home.title)
     val uistate by viewModel.uistate.collectAsState()
+    InstagramCopyScaffold(navController = navController, viewModel = viewModel, uiState = uistate)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun InstagramCopyScaffold(
+    navController: NavHostController,
+    viewModel: InstagramCopyViewModel,
+    uiState: UiState,
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = { InstagramCopyTopBar() },
+        bottomBar = { InstagramCopyBottomBar(navController = navController) }
+
+    ) {it ->
+        NavHost(
+            navController = navController,
+            startDestination = InstagramCopyScreen.Home.name,
+            modifier = modifier.padding(it)){
+            composable(InstagramCopyScreen.Home.name){
+                Text("Home Screen")
+            }
+            composable(InstagramCopyScreen.Explore.name){
+                Text("Explore")
+            }
+            composable(InstagramCopyScreen.Profile.name){
+                Text("Profile")
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InstagramCopyTopBar(
+    modifier: Modifier = Modifier,
+    navigation: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text("Instagram")
+        },
+        actions = {
+            Row{
+                IconButton(onClick = navigation) {
+                    Icon(painterResource(id = R.drawable.favorite), contentDescription = "Favorite Button")
+                }
+                IconButton(onClick = navigation) {
+                    Icon(painterResource(id = R.drawable.chat), contentDescription = "Chat Button")
+                }
+            } }
+    )
+}
+
+@Composable
+fun InstagramCopyBottomBar(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+){
+    BottomAppBar(modifier = modifier.fillMaxWidth(), contentPadding = PaddingValues(0.dp)) {
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .padding(0.dp) ,horizontalArrangement = Arrangement.SpaceEvenly){
+            IconButton(onClick = { navController.navigate(InstagramCopyScreen.Home.name) }){
+                Icon(painter = painterResource(id = R.drawable.home), contentDescription = "Home Button")
+            }
+            IconButton(onClick = { navController.navigate(InstagramCopyScreen.Explore.name) }) {
+                Icon(painter = painterResource(id = R.drawable.explore), contentDescription = "Explore Button")
+            }
+            IconButton(onClick = {navController.navigate(InstagramCopyScreen.Profile.name)}) {
+                Icon(painter = painterResource(id = R.drawable.person), contentDescription = "Profile Button")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    InstagramCopyTheme {
+        InstagramCopyApp()
+    }
 }
